2e3a7a84c355f6daeea6e890b495542357614232
djb77 <dwayne.bakewell@gmail.com>
2017-08-04 11:25:31 +1000

    LINUX: 3.18.30 Kernel Update
    Conflicts:
          arch/arm64/kernel/Makefile
          arch/arm64/kernel/psci.c
          sound/soc/codecs/wm_adsp.c
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -675,7 +675,7 @@ static int wm_adsp_fw_get(struct snd_kcontrol *kcontrol,
 	struct soc_enum *e = (struct soc_enum *)kcontrol->private_value;
 	struct wm_adsp *dsp = snd_soc_codec_get_drvdata(codec);
 
-	ucontrol->value.integer.value[0] = dsp[e->shift_l].fw;
+	ucontrol->value.enumerated.item[0] = dsp[e->shift_l].fw;
 
 	return 0;
 }
@@ -694,10 +694,10 @@ static int wm_adsp_fw_put(struct snd_kcontrol *kcontrol,
 	struct wm_adsp *dsp = &dsps[e->shift_l];
 	int ret;
 
-	if (ucontrol->value.integer.value[0] == dsp->fw)
+	if (ucontrol->value.enumerated.item[0] == dsp->fw)
 		return 0;
 
-	if (ucontrol->value.integer.value[0] >= dsp->num_firmwares)
+	if (ucontrol->value.enumerated.item[0] >= dsp->num_firmwares)
 		return -EINVAL;
 
 	switch (dsp->type) {
@@ -708,7 +708,7 @@ static int wm_adsp_fw_put(struct snd_kcontrol *kcontrol,
 	case WMFW_ADSP1:
 		if (dsp->running)
 			return -EBUSY;
-		dsp->fw = ucontrol->value.integer.value[0];
+		dsp->fw = ucontrol->value.enumerated.item[0];
 		return 0;
 	default:
 		return -EINVAL;
@@ -720,7 +720,7 @@ static int wm_adsp_fw_put(struct snd_kcontrol *kcontrol,
 		SND_SOC_DAPM_CLASS_RUNTIME);
 
 	wm_adsp2_shutdown_dsp(dsp);
-	dsp->fw = ucontrol->value.integer.value[0];
+	dsp->fw = ucontrol->value.enumerated.item[0];
 	wm_adsp2_set_dspclk(dsp, dsp->freq_cache);
 	queue_work(system_unbound_wq, &dsp->boot_work);
 	ret = wm_adsp2_start_dsp(dsp);
@@ -951,7 +951,7 @@ static int wm_coeff_put_acked(struct snd_kcontrol *kcontrol,
 {
 	struct wm_coeff_ctl *ctl =
 				(struct wm_coeff_ctl *)kcontrol->private_value;
-	unsigned int val = ucontrol->value.integer.value[0];
+	unsigned int val = ucontrol->value.enumerated.item[0];
 
 	if (!ctl->enabled)
 		return -EPERM;
@@ -1081,7 +1081,7 @@ static int wm_coeff_get_acked(struct snd_kcontrol *kcontrol,
 	 * the same event number again to the firmware). We therefore return 0,
 	 * meaning "no event" so valid event numbers will always be a change
 	 */
-	ucontrol->value.integer.value[0] = 0;
+	ucontrol->value.enumerated.item[0] = 0;
 
 	return 0;
 }
