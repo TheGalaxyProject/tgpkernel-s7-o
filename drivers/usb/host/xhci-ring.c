34aa0d9bd99ff2833382c1853ba003163e67949f
djb77 <dwayne.bakewell@gmail.com>
2017-08-04 11:38:26 +1000

    LINUX: 3.18.41 Kernel Update
    Conflicts:
          drivers/usb/host/xhci-ring.c
          net/ipv4/tcp_input.c
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -1498,6 +1498,13 @@ static void handle_cmd_completion(struct xhci_hcd *xhci,
 #endif
 		return;
 	}
+
+	if (cmd->command_trb != xhci->cmd_ring->dequeue) {
+		xhci_err(xhci,
+			 "Command completion event does not match command\n");
+		return;
+	}
+
 	/*
 	 * Host aborted the command ring, check if the current command was
 	 * supposed to be aborted, otherwise continue normally.
