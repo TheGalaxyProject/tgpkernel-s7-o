c632561d38f581deba5851e55430051ceaa908a1
djb77 <dwayne.bakewell@gmail.com>
2017-08-04 11:22:49 +1000

    LINUX: 3.18.17 Kernel Update
    Conflicts:
          drivers/hid/hid-ids.h
          include/net/sock.h
          net/ipv6/tcp_ipv6.c
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1993,8 +1993,10 @@ void task_numa_work(struct callback_head *work)
 		vma = mm->mmap;
 	}
 	for (; vma; vma = vma->vm_next) {
-		if (!vma_migratable(vma) || !vma_policy_mof(vma))
+		if (!vma_migratable(vma) || !vma_policy_mof(vma) ||
+		    is_vm_hugetlb_page(vma) || (vma->vm_flags & VM_MIXEDMAP)) {
 			continue;
+		}
 
 		/*
 		 * Shared library pages mapped by multiple processes are not
