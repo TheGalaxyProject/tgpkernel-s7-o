3844d3065760c2a4021dc110b9dbaf2316b29744
djb77 <dwayne.bakewell@gmail.com>
2017-10-27 22:41:11 +1100

    LINUX: 3.18.78 Kernel Update
    Conflicts:
          drivers/usb/core/config.c
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/usb/core/config.c
+++ drivers/usb/core/config.c
@@ -855,6 +855,7 @@ int usb_get_bos_descriptor(struct usb_device *dev)
 	for (i = 0; i < num; i++) {
 		buffer += length;
 		cap = (struct usb_dev_cap_header *)buffer;
+
 		if (total_len < sizeof(*cap) || total_len < cap->bLength) {
 			dev->bos->desc->bNumDeviceCaps = i;
 			break;
